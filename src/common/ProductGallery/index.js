6cb38be71b3ede58767598a00f7fe9c8a4581e72
Abdullah <chaudharyabdullahasif@gmail.com>
2023-02-22 20:30:24 +0500

    sdfsaf

--- src/common/ProductGallery/index.js
+++ src/common/ProductGallery/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Card from "./Cards"
 import {cardsData} from '../../utility/galleryProduct/index';
-import { Select, Space, Input,Col, Row } from "antd";
+import { Select ,Grid, Space, Input, Col, Row } from "antd";
 const handleChange = (value) => {
   console.log(`selected ${value}`);
 };
-function index({vendorValue}) {
+function Index({vendorValue}) {
+      const breakpoints = Grid.useBreakpoint();
+      const condition = breakpoints["xs"];
   return (
     <div className="gallery-container">
       <div className="gallery-container-up SectionPadding">
@@ -15,12 +17,9 @@ function index({vendorValue}) {
         <div className="gallery-container-up-right">
           <div className="gallery-container-up-right-items">
             <p>Sort By:</p>
-            <Space wrap>
               <Select
-                defaultValue="lucy"
-                style={{
-                  width: 120,
-                }}
+                className="gallery-container-up-right-items-select"
+                defaultValue={condition ? "Sort By" : "Jack"}
                 onChange={handleChange}
                 options={[
                   {
@@ -42,34 +41,33 @@ function index({vendorValue}) {
                   },
                 ]}
               />
-            </Space>
           </div>
           <div className="gallery-container-up-right-items">
             <p>View:</p>
-            <Input />
+            <Input placeholder={condition ? "View" : ""} />
           </div>
         </div>
       </div>
       <div className="gallery-container-down SectionPadding">
         <Row gutter={[16, { xs: 8, sm: 16, md: 24, lg: 32 }]}>
-            {cardsData.map((e,i)=>{
-              return (
-                <Col lg={6} md={12} sm={12} xs={24}>
-                  <Card
-                    key={i}
-                    price={e.desc}
-                    img={e.img}
-                    vendorValue={vendorValue}
-                    title={e.title}
-                    vendor={e.vender}
-                  />
-                </Col>
-              );
-            })}
+          {cardsData.map((e, i) => {
+            return (
+              <Col lg={6} md={12} sm={12} xs={24}>
+                <Card
+                  key={i}
+                  price={e.desc}
+                  img={e.img}
+                  vendorValue={vendorValue}
+                  title={e.title}
+                  vendor={e.vender}
+                />
+              </Col>
+            );
+          })}
         </Row>
       </div>
     </div>
   );
 }
 
-export default index
+export default Index
