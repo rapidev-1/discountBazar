e3b944c7fee9f646bfa1427537f2251f77e50100
rapidev-1 <rapidev.softwares@gmail.com>
2023-02-22 14:03:33 +0500

    udpate

--- src/common/ProductGallery/index.js
+++ src/common/ProductGallery/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import Card from "./Cards"
-import {cardsData} from '../../utility/galleryProduct/index';
-import { Select, Space, Input,Col, Row } from "antd";
+import { cardsData } from '../../utility/galleryProduct/index';
+import { Select, Space, Input, Col, Row } from "antd";
 const handleChange = (value) => {
   console.log(`selected ${value}`);
 };
-function index({vendorValue}) {
+function index({ vendorValue }) {
   return (
     <div className="gallery-container">
       <div className="gallery-container-up SectionPadding">
@@ -52,20 +52,20 @@ function index({vendorValue}) {
       </div>
       <div className="gallery-container-down SectionPadding">
         <Row gutter={[16, { xs: 8, sm: 16, md: 24, lg: 32 }]}>
-            {cardsData.map((e,i)=>{
-              return (
-                <Col lg={6} md={12} sm={12} xs={24}>
-                  <Card
-                    key={i}
-                    price={e.desc}
-                    img={e.img}
-                    vendorValue={vendorValue}
-                    title={e.title}
-                    vendor={e.vender}
-                  />
-                </Col>
-              );
-            })}
+          {cardsData.map((e, i) => {
+            return (
+              <Col xl={6} lg={8} md={12} sm={12} xs={24}>
+                <Card
+                  key={i}
+                  price={e.desc}
+                  img={e.img}
+                  vendorValue={vendorValue}
+                  title={e.title}
+                  vendor={e.vender}
+                />
+              </Col>
+            );
+          })}
         </Row>
       </div>
     </div>
