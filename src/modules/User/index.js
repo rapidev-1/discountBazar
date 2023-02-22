f964828e684af8cf1643fd846d4f17e931cad7a8
rapidev-1 <rapidev.softwares@gmail.com>
2023-02-23 00:02:17 +0500

    React.Laxy

--- src/modules/User/index.js
+++ src/modules/User/index.js
@@ -3,22 +3,26 @@ import { Route, Routes } from "react-router-dom"
 import { LandingPage, LoginPage, MultiShop, SignupPage, SingleShop } from './components'
 import { AboutPage } from "./components";
 import UserLayout from '../../common/layout/UserLayout'
-import ErrorPage from "../../common/Errors/404"
-import SearchPage from './components/SearchPage';
+import Loader from '../../common/Loader';
+const ErrorPage = React.lazy(() => import("../../common/Errors/404"))
+const SearchPage = React.lazy(() => import('./components/SearchPage'));
+
 function UserRoutes() {
   return (
-    <Routes>
-      <Route path="/*" element={<UserLayout />}>
-        <Route index element={<LandingPage />} />
-        <Route path="about" element={<AboutPage />} />
-        <Route path="shop" element={<MultiShop />} />
-        <Route path="vendorshop/:username" element={<SingleShop />} />
-        <Route path="search" element={<SearchPage />} />
-        <Route path="*" element={<ErrorPage />} />
-      </Route>
-      <Route path='login' element={<LoginPage />} />
-      <Route path='signup' element={<SignupPage />} />
-    </Routes>
+    <React.Suspense fallback={<Loader />}>
+      <Routes>
+        <Route path="/*" element={<UserLayout />}>
+          <Route index element={<LandingPage />} />
+          <Route path="about" element={<AboutPage />} />
+          <Route path="shop" element={<MultiShop />} />
+          <Route path="vendorshop/:username" element={<SingleShop />} />
+          <Route path="search" element={<SearchPage />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Route>
+        <Route path='login' element={<LoginPage />} />
+        <Route path='signup' element={<SignupPage />} />
+      </Routes>
+    </React.Suspense>
   );
 }
 
