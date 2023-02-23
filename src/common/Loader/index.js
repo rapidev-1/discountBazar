db06aa8af1a0a5aee05761ee63109802cf0204e0
rapidev-1 <rapidev.softwares@gmail.com>
2023-02-23 10:02:45 +0500

    ok

--- src/common/Loader/index.js
+++ src/common/Loader/index.js
@@ -1,9 +1,26 @@
-import { Row, Spin } from 'antd'
-import React, { useState } from 'react'
+import { Grid, Row, Spin } from 'antd'
+import React, { useEffect, useState } from 'react'
 
 function Loader() {
     const [loading, setloading] = useState(true)
-    window.addEventListener("load", () => setloading(false))
+    const breakpoints = Grid.useBreakpoint()
+
+
+    useEffect(() => {
+        var timeout;
+        if (breakpoints['xs'])
+            timeout = setTimeout(() => {
+                setloading(false)
+                window.removeEventListener("load")
+            }, 3000);
+        else {
+            window.addEventListener("load", () => setloading(false))
+        }
+        return () => {
+            window.removeEventListener("load")
+            clearTimeout(timeout)
+        }
+    }, [breakpoints])
     if (!loading) return null
     document.body.style.height = undefined
     return (
