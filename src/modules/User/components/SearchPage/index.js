86d3b6a545ae68980d57b80d153b8ac38e039f97
rapidev-1 <rapidev.softwares@gmail.com>
2023-02-24 14:19:55 +0500

    new loader

--- src/modules/User/components/SearchPage/index.js
+++ src/modules/User/components/SearchPage/index.js
@@ -1,12 +1,26 @@
+import { Col, Row } from 'antd'
 import React, { Fragment } from 'react'
 import Banner from "../../../../common/Banner"
+import SearchPageProductsCard from './subcomponents/ProductsCard'
 function SearchPage() {
     return (
         <Fragment>
             <Banner title={"search"} />
-            oakdfoaf
-            oakfdaodsjf
-            oajdjfaklj
+            <Row
+                gutter={[20, 15]}
+                justify={"space-between"}
+                className={"section"}>
+                {
+                    Array(10).fill(null).map((e, i) => {
+                        return (
+                            <Col span={24}>
+                                <SearchPageProductsCard />
+                            </Col>
+                        )
+                    })
+                }
+
+            </Row>
         </Fragment>
     )
 }
---
+++ src/modules/User/components/SearchPage/subcomponents/Vendor_ShopCard/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Vendor_ShopCard() {
+    return (
+        <div>Vendor_ShopCard</div>
+    )
+}
+
+export default Vendor_ShopCard
