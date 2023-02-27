d09c827a86e63267f677760eef244576635d4735
rapidev-1 <rapidev.softwares@gmail.com>
2023-02-27 18:50:17 +0500

    update

--- src/modules/User/components/ProductCompare/Heading/CompareSearchBar/index.js
+++ src/modules/User/components/ProductCompare/Heading/CompareSearchBar/index.js
@@ -1,10 +1,64 @@
-import { Row } from 'antd'
+import { Col, Input, Row, Select } from 'antd'
 import React from 'react'
 
 function CompareSearchBar() {
     return (
-        <Row className='SectionPadding'>
+        <Row className='SectionPadding CompareSearchBar'>
+            <Col className='CompareSearchBarCol' span={24}>
+                <Row gutter={[20, 10]} align="middle" justify={"space-between"}>
+                    <Col md={10} sm={24} xs={24}>
+                        <Row
+                            align={"middle"}
+                            gutter={[
+                                { lg: 20, md: 10, sm: 10, xs: 10 },
+                                { lg: 20, md: 10, sm: 10, xs: 10 }]}>
+                            <Col sm={12} xs={24}>
+                                <Input
+                                    placeholder='search product...'
+                                    type='text' />
+                            </Col>
+                            <Col sm={12} xs={24}>
+                                <Input
+                                    placeholder='search product...'
+                                    type='text' />
+                            </Col>
+                        </Row>
+                    </Col>
+                    <Col lg={8} md={10} sm={24} xs={24}>
+                        <Row align={"middle"} gutter={[10, 10]}>
+                            <Col sm={8} xs={24}>
+                                <Select
+                                    style={{ width: "100%" }}
+                                    options={
+                                        Array(20).fill(null).map((e, i) => {
+                                            return {
+                                                label: 'less than ' + (i + 1) * 100,
+                                                value: (i + 1) * 100
+                                            }
+                                        })
 
+                                    }
+                                    placeholder='Price'
+                                    type='text' />
+                            </Col>
+                            <Col sm={8} xs={24}>
+                                <Select
+                                    style={{ width: "100%" }}
+                                    options={[
+                                        { label: "Online", value: "online" },
+                                        { label: "Local", value: "local" }]}
+                                    placeholder='Vendor Type'
+                                    type='text' />
+                            </Col>
+                            <Col sm={8} xs={24}>
+                                <Input
+                                    placeholder='Region'
+                                    type='text' />
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
+            </Col>
         </Row>
     )
 }
