5a68571deb2a316f8b2a4d27af52c7a7a7f6d6a4
rapidev-1 <rapidev.softwares@gmail.com>
2023-02-27 13:47:45 +0500

    upate

--- src/modules/User/components/ProductPage/subcomponent/TabsInfo/tab4/map.js
+++ src/modules/User/components/ProductPage/subcomponent/TabsInfo/tab4/map.js
@@ -1,4 +1,7 @@
+import { Popover } from 'antd'
+import { useEffect, useRef, useState } from 'react'
 import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet'
+import DiscountBazarIcon from '../../../../../../../common/Icons'
 import RoutingMachine from './RouteMachine'
 const Map = () => {
   const lat1 = 33.683739
@@ -7,20 +10,30 @@ const Map = () => {
   const lng2 = 72.98944
   const center = [(lat1 + lat2) / 2, (lng1 + lng2) / 2]
   const timer = 3000
+  const [reCalc, setReCalc] = useState(false)
+  const centered = useRef(false)
 
+  const ReaCalculate = () => {
+    const map = useMap();
+    map.setView([lat1, lng1], 16);
+    setReCalc(false)
+    return null;
+  }
 
   function ChangeView({ center, zoom }) {
+    const map = useMap();
     setTimeout(() => {
       map.setView(center, zoom);
     }, timer);
-    const map = useMap();
-
+    centered.current = true
     return null;
   }
+
 
   let markericons = ".leaflet-marker-icon"
   markericons = document.querySelectorAll(markericons)
   // markericons is array of marker icons in map, we can change icon by markericons[0].src=""
+
   return (
     <MapContainer
       trackResize
@@ -30,23 +43,36 @@ const Map = () => {
       }}
       center={center}
       scrollWheelZoom={true} >
-      <ChangeView center={[lat1, lng1]} zoom={18} />
+
 
       <TileLayer
         attribution='Discount Bazar'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       <Marker position={[lat1, lng1]} >
-        <Popup className='youiconmap'>
+        <Popup
+          closeButton={false}
+          className='youiconmap'>
           (You)
         </Popup>
       </Marker>
-      <Marker position={[lat2, lng2]}>
-        <Popup className='vendoriconmap'>
+      <Marker autoPan={true} position={[lat2, lng2]}>
+        <Popup
+          closeButton={false}
+          className='vendoriconmap'>
           (Vendor)
         </Popup>
       </Marker>
+
+      <Popover title="User" trigger={"hover"}>
+        <DiscountBazarIcon
+          icon={"user"}
+          className={"recalculate"}
+          onClick={() => setReCalc(e => !e)} />
+      </Popover>
       <RoutingMachine destination={[lat2, lng2]} source={[lat1, lng1]} />
+      {centered.current === false && <ChangeView center={[lat1, lng1]} zoom={18} />}
+      {reCalc && <ReaCalculate />}
     </MapContainer >
   )
 }
