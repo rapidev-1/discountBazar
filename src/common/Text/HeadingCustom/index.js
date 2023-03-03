2fc0a0437840ae6d9d1c5f8d199577530d86f6e1
rapidev-1 <rapidev.softwares@gmail.com>
2023-03-03 23:21:24 +0500

    ok

--- src/common/Text/HeadingCustom/index.js
+++ src/common/Text/HeadingCustom/index.js
@@ -1,7 +1,7 @@
 import { Grid, Row, Typography } from 'antd'
 import React from 'react'
 
-function HeadingCustom({ heading, level, small }) {
+function HeadingCustom({ heading, level, small, className }) {
     const breakpoints = Grid.useBreakpoint()
     const condition = breakpoints['sm'] || breakpoints['xs']
     return (
@@ -10,7 +10,7 @@ function HeadingCustom({ heading, level, small }) {
                 <Typography.Title
                     level={level ? level : 2}
                     ellipsis={{ rows: condition ? 2 : 1 }}
-                    className={`HeadingDark HeadingCustom ${small ? "small" : ""}`}>
+                    className={`HeadingDark HeadingCustom ${small ? "small" : className ? className : ""}`}>
                     {heading}
                 </Typography.Title>
             </Typography>
