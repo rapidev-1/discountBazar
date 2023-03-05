da0a36f8dda48b6bca843ea6987df472ae805d96
rapidev-1 <rapidev.softwares@gmail.com>
2023-03-05 18:53:57 +0500

    update

--- src/modules/User/components/profile/index.js
+++ src/modules/User/components/profile/index.js
@@ -1,16 +1,19 @@
 import { Col, Row } from 'antd'
 import React, { Fragment } from 'react'
 import Banner from "../../../../common/Banner"
+import ProfileMenu from './menu'
 function UserProfile() {
     return (
         <Fragment>
             <Banner title={"profile"} />
             <Row
-                gutter={[20, 15]}
                 justify={"space-between"}
-                className={"section"}>
-                <Col>
-                    profile Page
+                className={"section profileRow"}>
+                <Col md={6} sm={24}>
+                    <ProfileMenu />
+                </Col>
+                <Col md={17} sm={24}>
+                    col2
                 </Col>
             </Row>
         </Fragment>
