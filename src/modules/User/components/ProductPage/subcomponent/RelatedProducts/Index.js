e5b05f447969fd7cc4632dad6063a68660682b48
Abdullah <chaudharyabdullahasif@gmail.com>
2023-03-07 23:12:39 +0500

    Sdsa

--- src/modules/User/components/ProductPage/subcomponent/RelatedProducts/Index.js
+++ src/modules/User/components/ProductPage/subcomponent/RelatedProducts/Index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Col, Row } from "antd";
 import { cards } from '../../../../../../utility/galleryProduct';
 import Card from "../../../../../../common/Product/cards"
-
+ 
 function Index({ vendorValue }) {
   return (
     <div className="SectionPadding related-container">
